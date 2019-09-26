092193be934b2e17e577fe2c29f6c75cc08787aa
andre <andre@DESKTOP-2GTLJFE>
2019-09-25 21:14:56 -0600

    Primera subida github proyecto gym

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: andre
+ *
+ * Created on 25 de septiembre de 2019, 09:13 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+//Prueba 
+    return 0;
+}
+
